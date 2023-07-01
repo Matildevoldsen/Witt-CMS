20a8c3e7db5b5a5b1845f848b8d550d95bc92b0b
Matilde Enevoldsen <matildeene@outlook.com>
2023-07-01 16:25:35 +0100

    Add auth logo based SiteSettings model

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -10,10 +10,11 @@ defineProps({
     <nav class="relative flex justify-center">
         <Link
             v-for="link in pagination.links"
-            :key="link.label"
+            :key="link?.label"
+            preserve-scroll
             class="flex items-center justify-center px-3 py-2 text-sm rounded-lg dark:text-gray-200 text-gray-600"
-            :class="{'bg-gray-200 dark:bg-gray-800': link.active, 'opacity-50 dark:opacity-50': !link.url}"
-            :href="link.url ?? ''"
-            v-html="link.label"/>
+            :class="{'bg-gray-200 dark:bg-gray-800': link?.active, 'opacity-50 dark:opacity-50': !link?.url}"
+            :href="link?.url ?? ''"
+            v-html="link?.label"/>
     </nav>
 </template>
