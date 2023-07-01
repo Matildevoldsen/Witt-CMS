8b33203c0998da8384171eeeaa16752fcd031a04
Matilde Enevoldsen <matildeene@outlook.com>
2023-07-01 16:21:27 +0100

    Remove setTheme and add new CSS styles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full bg-white" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,23 +12,11 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <script>
-            window.addEventListener('load', function () {
-                // Check if the browser's theme is set to dark mode
-                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                    // If dark mode is set, create a cookie named "theme" with the value "dark"
-                    document.cookie = "theme=dark; path=/";
-                } else {
-                    // If dark mode is not set, create a cookie named "theme" with the value "light"
-                    document.cookie = "theme=light; path=/";
-                }
-            });
-        </script>
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans h-full antialiased bg-white dark:bg-gray-900">
         @inertia
     </body>
 </html>
