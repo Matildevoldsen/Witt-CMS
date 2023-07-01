8b0e0224f2fad604e10af8ee48f521bdf6538101
Matilde Enevoldsen <matildeene@outlook.com>
2023-07-01 16:23:36 +0100

    Add fillable to SiteSettings

--- app/Models/SiteSettings.php
+++ app/Models/SiteSettings.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class SiteSettings extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'description',
+        'logo_path',
+    ];
 }
