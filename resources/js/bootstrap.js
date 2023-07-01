94f5045329d1f38786924ecb5445e59d5b7302d5
Matilde Enevoldsen <matildeene@outlook.com>
2023-07-01 16:17:45 +0100

    Add echo.js support

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,6 +5,7 @@
  */
 
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -15,18 +16,34 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+import Pusher from 'pusher-js';
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    wsHost: window.location.hostname,
+    wssPort: 6001,
+    authorizer: (channel, options) => {
+        return {
+            authorize: (socketId, callback) => {
+                callback(false, {}); // Just auth every request
+            }
+        };
+    },
+});
+window.addEventListener('load', function () {
+    // Check if the browser's theme is set to dark mode
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // If dark mode is set, create a cookie named "theme" with the value "dark"
+        document.cookie = "theme=dark; path=/";
+    } else {
+        // If dark mode is not set, create a cookie named "theme" with the value "light"
+        document.cookie = "theme=light; path=/";
+    }
+});
