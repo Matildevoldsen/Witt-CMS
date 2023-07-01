edaf7c5f41bc4d4e424c3e397152a8c5b6db6e49
Matilde Enevoldsen <matildeene@outlook.com>
2023-07-01 16:20:48 +0100

    Add dashboardLayout

---
+++ resources/js/Layouts/DashboardLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <SidebarNavigation>
+        <slot/>
+    </SidebarNavigation>
+</template>
+<script setup>
+import ApplicationLogo from "../Components/ApplicationLogo.vue";
+import CSVImporter from "../Components/CSVImporter.vue";
+import SidebarNavigation from "../Components/Dashboard/SidebarNavigation.vue";
+</script>
