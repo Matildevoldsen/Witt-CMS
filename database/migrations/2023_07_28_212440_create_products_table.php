760bab43126f7ff9d076bd7b493c71dd17f8d61d
Matilde Enevoldsen <matildeene@outlook.com>
2023-07-31 21:26:08 +0100

    Make product nullable

--- database/migrations/2023_07_28_212440_create_products_table.php
+++ database/migrations/2023_07_28_212440_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->string('slug')->unique();
+            $table->integer('price')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('live_at')->nullable();
             $table->timestamps();
         });
     }
